aa251e0a15f012585baaedde28c2fd4382ee9bd9
m-woo <mark.chi.woo@gmail.com>
2018-05-29 23:25:29 -0700

    wedding website: pricing, left/right margin, extra parallax, top left logo, about section

--- script.js
+++ script.js
@@ -16,3 +16,56 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     }
   }
 });
+
+
+window.addEventListener('message', function(e) {
+  var data = e.data.split('-'),
+    scroll_height = data[0],
+    iframe_id = data[1];
+
+  // Check message from which iframe
+  if(iframe_id == 'iframe1')
+    document.getElementById('iframe-container-1').style.height = scroll_height + 'px'; 
+  else if(iframe_id == 'iframe2')
+    document.getElementById('iframe-container-2').style.height = scroll_height + 'px'; 
+} , false);
+
+
+
+
+
+
+
+
+
+
+    $(function(){
+    
+        var iFrames = $('iframe');
+      
+      function iResize() {
+      
+        for (var i = 0, j = iFrames.length; i < j; i++) {
+          iFrames[i].style.height = iFrames[i].contentWindow.document.body.offsetHeight + 'px';}
+          }
+          
+          if ($.browser.safari || $.browser.opera) { 
+          
+             iFrames.load(function(){
+                 setTimeout(iResize, 0);
+               });
+            
+             for (var i = 0, j = iFrames.length; i < j; i++) {
+              var iSource = iFrames[i].src;
+              iFrames[i].src = '';
+              iFrames[i].src = iSource;
+               }
+               
+          } else {
+             iFrames.load(function() { 
+                 this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
+             });
+          }
+        
+        });
+
