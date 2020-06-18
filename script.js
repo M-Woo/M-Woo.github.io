47351919b299ca21ebe6ebec1f06093a923d68c1
M-Woo <mark.chi.woo@gmail.com>
2020-06-19 00:18:41 +0800

    1. Consolidated NavTop + NavBot 2. Reformatted all pages 3. Added Fade when scroll to NavTop 4. Added AOS 5. Changed About Photo 6. Reformatted NavTop elements

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// $(document).ready(function() {
-//     $('#trigger').click(function(){
-//       $("#dialog").dialog();
-//     }); 
-//   });   
+$(document).ready(function() {
+    $('#trigger').click(function(){
+      $("#dialog").dialog();
+    }); 
+  });   
 
 $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -16,36 +16,16 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     }
   }
 });
+      
 
-// NAVBAR-------------------------------------------------
+// TITLE FADE OUT TRANSITION----------------------------------
 
-$(function($) { 
-  $(document).ready(function(){
-    $("#nav-placeholder").load("nav.html");
+
+$(window).scroll(function(){
+    $(".fadeOut").css("opacity", 1 - $(window).scrollTop() / 150);
   });
-})
 
-
-
-
-// TITLE TRANSITION----------------------------------
-
-$(function($) {          
-    $(document).ready(function(){                    
-        $(window).scroll(function(){                          
-            if ($(this).scrollTop() > 600) {
-                $('#fade').fadeIn(1500);
-            } else {
-                $('#fade').fadeOut(1500);
-            }
-        });
-    });
-})(jQuery);
-
-
-
-
-
+// TITLE FADE IN TRANSITION----------------------------------
 
 
 
